3aa6893ac3bf0e7d3326089b14f794a874db3a29
Michael-RealT <Michael@realt.co>
2025-01-19 22:25:56 +0100

    refactor: standardize address formatting in optionsModifiers.ts exclusion list

--- src/configs/optionsModifiers.ts
+++ src/configs/optionsModifiers.ts
@@ -19,19 +19,19 @@ const getDexPoolAddresses = () => {
  * !! Adresses en minuscule
  */
 const excludeAddressREG = [
-  "0xc49bb05ac57f371bbc30ea0b7b0679343cad71c0", // Safe Marketing
-  "0xa99302f22f646c075413c69f2565fe69b5f5829a", // Vault Genesis
-  "0x3f2d192f64020da31d44289d62db82ade6abee6c", // Tresorie DAO Timelocker
-  "0xe1877d33471e37fe0f62d20e60c469eff83fb4a0", // Vault incentives
-  "0xba12222222228d8ba445958a75a0704d566bf2c8", // Vault Balancer
-  "0x0212b941cb5c80c9fcda00035d126ac067169e72", // Safe DAO liquidity
-  "0x3A37b789D3117F9B4f92F563358D70336F893E21", // NFT Collector
-  "0x65CFC5514ABa2eDB84ecdE3757C7647D2Df4E6Af", // NFT Activity
-  "0x91036536da4c5155D1d05bA235B93192d92DaE1d", // NFT Cityzen
-  "0xDff7A394Bd57593b3caA8f601e0A162bf711655E", // Contribution Communautaire
-  "0x6B85a87d8990e77A86ab16A44b162de48BFb64E9", // Provision D'ajustement
-  "0x9a760aa1fe631fd9ac0aee0965736121c7c132cc", // MtP OnOff Ramp
-  "0xb65a382e536cf80a792e10b1ff24f3c04845d4a3", // Realt gestion Budget MtP
+  "0xc49bb05ac57f371bbc30ea0b7b0679343cad71c0", // safe marketing
+  "0xa99302f22f646c075413c69f2565fe69b5f5829a", // vault genesis
+  "0x3f2d192f64020da31d44289d62db82ade6abee6c", // tresorie dao timelocker
+  "0xe1877d33471e37fe0f62d20e60c469eff83fb4a0", // vault incentives
+  "0xba12222222228d8ba445958a75a0704d566bf2c8", // vault balancer
+  "0x0212b941cb5c80c9fcda00035d126ac067169e72", // safe dao liquidity
+  "0x3a37b789d3117f9b4f92f563358d70336f893e21", // nft collector
+  "0x65cfc5514aba2edb84ecde3757c7647d2df4e6af", // nft activity
+  "0x91036536da4c5155d1d05ba235b93192d92dae1d", // nft cityzen
+  "0xdff7a394bd57593b3caa8f601e0a162bf711655e", // contribution communautaire
+  "0x6b85a87d8990e77a86ab16a44b162de48bfb64e9", // provision d'ajustement
+  "0x9a760aa1fe631fd9ac0aee0965736121c7c132cc", // mtp onoff ramp
+  "0xb65a382e536cf80a792e10b1ff24f3c04845d4a3", // realt gestion budget mtp
   ...getDexPoolAddresses(),
 ];
 
